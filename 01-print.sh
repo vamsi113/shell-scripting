45c17ad44d13635e043463c27439d3537b643c15
vamsi113 <krishultimate5@gmail.com>
2023-02-21 18:41:13 -0500

    adding catalogue script |variables|print|redirectors|exit status|Sed Editor|if conditions|exit command| exit 1|if con for root user| if con for app user

--- 01-print.sh
+++ 01-print.sh
@@ -1,7 +1,7 @@
 
 
 ## problems raised while shell- scripting:
-#Ensured the script should run only if it is a root user/sudo previleges. --- sol run with sudo permissions
-#if any command is a failure then i need to stop the script there and then --- sol -conditions
-#rather than printing 0 and 1 or  something i printed success or failure  --- sol -- conditions
+#Ensured the script should run only if it is a root user/sudo previleges. --- solvd( run with sudo permissions)
+#if any command is a failure then i need to stop the script there and then --- solved( -exit status and exit command)
+#rather than printing 0 and 1 or  something i printed success or failure  --- solved(if conditons)
 #re-run of automation should never fail
