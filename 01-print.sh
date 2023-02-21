2c3465269f0faecf78177a5fec691280aa972b7d
vamsi113 <krishultimate5@gmail.com>
2023-02-21 17:44:47 -0500

    adding moongodb script |variables|print|redirectors|exit status|Sed Editor

--- 01-print.sh
+++ 01-print.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
 
-echo  Hello World
+
+## problems raised while shell- scripting:
+#Ensured the script should run only if it is a root user/sudo previleges. --- sol run with sudo permissions
+#if any command is a failure then i need to stop the script there and then --- sol -conditions
+#rather than printing 0 and 1 or  something i printed success or failure  --- sol -- conditions
+#re-run of automation should never fail
