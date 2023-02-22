ccb8b028733f50b20ce94ec3f2f933577559234d
vamsi113 <krishultimate5@gmail.com>
2023-02-22 13:44:33 -0500

    adding catalogue script |variables|print|redirectors|exit status|Sed Editor|if conditions|exit command| exit 1|if con for root user| if con for app user| remove old content|color added |maintained dry code using functions |

--- 01-print.sh
+++ 01-print.sh
@@ -4,4 +4,5 @@
 #Ensured the script should run only if it is a root user/sudo previleges. --- solvd( run with sudo permissions)
 #if any command is a failure then i need to stop the script there and then --- solved( -exit status and exit command)
 #rather than printing 0 and 1 or  something i printed success or failure  --- solved(if conditons)
-#re-run of automation should never fail
+#re-run of automation should never fail --solved(conditions)-- raised duplication issue
+## maintaining dry code failed(duplication issue)-- sloved(functions)
